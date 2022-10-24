ef027e29e1d2bb00fb6c24b851e6ea72bf5be7c8
Aman Deep <deep.aman1729@gmail.com>
2022-10-24 17:36:25 +0530

    main.js is updated

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 import "./style.css";
 import mapObj from "./js/map";
-import VectorSource from "ol/source/Vector";
-import VectorLayer from "ol/layer/Vector";
 import Draw, { createBox } from "ol/interaction/Draw";
-import Style from "ol/style/Style";
-import Icon from "ol/style/Icon";
-import Fill from "ol/style/Fill";
-import Stroke from "ol/style/Stroke";
-import GeoJSON from 'ol/format/GeoJSON'
-
+import {
+  lineSource,
+  lineVector,
+  squareSource,
+  squareVector,
+  polygonSource,
+  polygonVector,
+  markerSource,
+  markerVector,
+} from "./js/shapes";
+import { Feature } from "ol";
+import GeoJSON from "ol/format/GeoJSON";
+import KML from "ol/format/KML";
+import TopoJSON from "ol/format/TopoJSON";
+import {
+  downloadKml,
+  downloadgeoJson,
+  downloadtopoJson,
+} from "./js/downloader";
 
 // layer switcher
 const layers = document.querySelector("#layers");
@@ -23,105 +34,162 @@ layers.addEventListener("change", (e) => {
 
 // draw shapes
 
-const styles = {
-  Point: new Style({
-    image: new Icon({
-      anchor: [0.5, 46],
-      anchorXUnits: "fraction",
-      anchorYUnits: "pixels",
-      src: "/marker.png",
-    }),
-  }),
-  LineString: new Style({
-    fill: new Fill({
-      color: "lightred",
-    }),
-    stroke: new Stroke({
-      color: "red",
-    }),
-  }),
-};
+window.source = markerSource;
 
 const shapes = document.querySelector("#shapes");
-let draw;
-window.sourceInteraction = new VectorSource({format:GeoJSON, wrapX: false });
-const styleFunction = (feature) => {
-  console.log(styles[feature.getGeometry().getType()]);
-};
-
-window.vector = new VectorLayer({
-  source: sourceInteraction,
-  map: mapObj,
-  style:new Style({
-    fill: new Fill({
-      color: 'rgba(255 , 0, 0, 0.1)',
-    }),
-    stroke: new Stroke({
-      color: 'red',
-    }),
-  }),
-});
+let lineDraw, markerDraw, squareDraw, polygonDraw;
 
 shapes.addEventListener("change", (e) => {
-  mapObj.removeInteraction(draw);
+  mapObj.removeInteraction(lineDraw);
+  mapObj.removeInteraction(markerDraw);
+  mapObj.removeInteraction(squareDraw);
+  mapObj.removeInteraction(polygonDraw);
   let type = e.target.value;
-  let geometryFunction;
 
   switch (type) {
     case "line":
       type = "LineString";
+
+      lineDraw = new Draw({
+        source: lineSource,
+        type: type,
+        stopClick: false,
+      });
+      mapObj.addInteraction(lineDraw);
+
+      lineDraw.on("drawend", (e) => {
+        lineDraw.setActive(false);
+      });
+
       break;
     case "square":
       type = "Circle";
-      geometryFunction = createBox();
+
+      squareDraw = new Draw({
+        source: squareSource,
+        type: type,
+        stopClick: false,
+        geometryFunction: createBox(),
+      });
+      mapObj.addInteraction(squareDraw);
+
+      squareDraw.on("drawend", (e) => {
+        squareDraw.setActive(false);
+      });
 
       break;
 
     case "polygon":
       type = "Polygon";
+      polygonDraw = new Draw({
+        source: polygonSource,
+        type: type,
+        stopClick: false,
+      });
+
+      mapObj.addInteraction(polygonDraw);
+      polygonDraw.on("drawend", (e) => {
+        polygonDraw.setActive(false);
+      });
       break;
 
     case "point":
       type = "Point";
+      markerDraw = new Draw({
+        source: markerSource,
+        type: type,
+        stopClick: false,
+      });
+      mapObj.addInteraction(markerDraw);
+      markerDraw.on("drawend", (e) => {
+        markerDraw.setActive(false);
+      });
+
       break;
 
     default:
-      console.log("");
+      console.log(" Not in option ");
       break;
   }
+});
 
-  if (geometryFunction === undefined) {
-    draw = new Draw({
-      source: sourceInteraction,
-      type: type,
-      stopClick: true,
-      style:new Style({
-        fill: new Fill({
-          color: 'rgba(255 , 0, 0, 0.1)',
-        }),
-        stroke: new Stroke({
-          color: 'red',
-          width:"3px"
-        }),
-      }),
-    })
-  } else {
-    draw = new Draw({
-      source: sourceInteraction,
-      type: type,
-      geometryFunction: geometryFunction,
-      stopClick: true,
-      style:new Style({
-        fill: new Fill({
-          color: 'red',
-        }),
-        stroke: new Stroke({
-          color: 'red',
-        }),
-      }),
-    });
+// download data
+
+const getKML = (features) => {
+  const formatKml = new KML();
+  const kml = formatKml.writeFeatures(features, {
+    featureProjection: "EPSG:3857",
+  });
+  return kml;
+};
+
+const getGeoJson = (features) => {
+  const formatGeojson = new GeoJSON();
+  const geojson = formatGeojson.writeFeatures(features, {
+    featureProjection: "EPSG:3857",
+  });
+  const parseJson = JSON.parse(geojson);
+  const f = parseJson.features;
+  for (const i of f) {
+    i.properties = {};
   }
+  return JSON.stringify(parseJson);
+};
 
-  console.log(sourceInteraction.getFeatures());
-  mapObj.addInteraction(draw);
+const getTopoJson = (features) => {
+  const formattopojson = new TopoJSON();
+  const topojson = formattopojson.writeFeatures(features);
+  return topojson;
+};
+
+const getAllfeatures = (lineVal, squareVal, polygonVal, markerVal) => {
+  let features = [];
+
+  lineVal.forEach((l) => {
+    features.push(l);
+  });
+  squareVal.forEach((l) => {
+    features.push(l);
+  });
+  polygonVal.forEach((l) => {
+    features.push(l);
+  });
+  markerVal.forEach((l) => {
+    features.push(l);
+  });
+
+  return features;
+};
+
+const format = document.querySelector("#format");
+
+format.addEventListener("change", (e) => {
+  const lineVal = lineSource.getFeatures();
+  const squareVal = squareSource.getFeatures();
+  const polygonVal = polygonSource.getFeatures();
+  const markerVal = markerSource.getFeatures();
+  const allFeatures = getAllfeatures(lineVal, squareVal, polygonVal, markerVal);
+
+  const value = e.target.value;
+  switch (value) {
+    case "kml":
+      const kml = getKML(allFeatures);
+      downloadKml(kml);
+      break;
+
+    case "topojson":
+      const topojson = getTopoJson(allFeatures);
+      console.log(topojson);
+      // downloadtopoJson(topojson)
+      break;
+
+    case "geojson":
+      const geojson = getGeoJson(allFeatures);
+      downloadgeoJson(geojson);
+      break;
+
+    default:
+      console.log("value error");
+      break;
+  }
 });
